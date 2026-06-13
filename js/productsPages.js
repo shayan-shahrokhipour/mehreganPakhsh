4719dcd4f9b5145410970566a42bdc2f0dee0dd9
oneandonlyshayanshahrokhi <shayanshahroukhipour@gmail.com>
2026-06-13 03:32:52 -0700

    add something new

--- js/productsPages.js
+++ js/productsPages.js
@@ -42,3 +42,10 @@ product.innerHTML=filtered.map(item=>`<div class="productsCard">
     </div>`).join('')
 console.log(product);
 
+//fetch api
+async function data() {    
+    const response = await fetch('http://192.168.1.103:8000/products/')
+    const json = await response.json()
+    
+}
+data()
