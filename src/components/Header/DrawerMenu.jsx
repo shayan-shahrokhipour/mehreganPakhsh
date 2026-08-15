527b2299bbd34d7224b44d48963378b29cd50f17
shayan-shahrokhipour <shayanshahroukhipour@gmail.com>
2026-08-14 22:08:23 -0700

    add increase quantity

--- src/components/Header/DrawerMenu.jsx
+++ src/components/Header/DrawerMenu.jsx
@@ -1,23 +1,33 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import styles from "../Header/headerstyles/header.module.css";
+//Context
+import { BuyContext } from "../../context/CartContext";
+//react Icon
 import { IoClose } from "react-icons/io5";
-import { BuyContext } from '../../context/CartContext';
-const DrawerMenu = ({state,drawerHandeler,modalHandeler}) => {
-  
-const {selectedItems} = useContext(BuyContext)
-   
- 
+import { FaTrash } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa6";
+const DrawerMenu = ({ state, drawerHandeler, modalHandeler }) => {
+  const { selectedItems,increaseThequantity} = useContext(BuyContext);
    
   return (
-   <div className={`${styles.drawerMenu} ${state === true ? styles.drawerMenuActive : ""}`}>
-       <IoClose onClick={drawerHandeler} className={styles.closeIcon}/>
-       {selectedItems.map(item => <div key={item.id}>
-         <p>{item.name}</p>
-         <p>{item.price}</p>
-         
-       </div>)}
+    <div
+      className={`${styles.drawerMenu} ${state === true ? styles.drawerMenuActive : ""}`}
+    >
+      <IoClose onClick={drawerHandeler} className={styles.closeIcon} />
+      {selectedItems.map((item) => (
+        <div className={styles.selectedProducts} key={item.id}>
+          <p>{item.name}</p>
+          <p>مبلغ قابل پرداخت :{item.price.toLocaleString("fa-IR")}</p>
+          <div className={styles.buttonHolder}>
+            <FaPlus onClick={()=>increaseThequantity(item.id)} className={styles.iconsBuy}/>
+            <p>{item.quantity}</p>
+          {item.quantity===1 ? <FaTrash className={styles.iconsBuy}/> : <FaMinus  className={styles.iconsBuy}/>}
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default DrawerMenu
+export default DrawerMenu;
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,38 +4,52 @@ import { ProductContext } from './ContextProvider'
 
 
  const initialArg ={
-  selectedItems:[]
+  selectedItems:[],
+
  }
 
 const reducer=(state,action)=>{
-   if(action.type==="AddItem"){
-    console.log(state);
+   
     
-       return {...state,selectedItems:[...state.selectedItems,action.payload]};
+  if(action.type==="AddItem"){
+    
+     return {...state,selectedItems:[...state.selectedItems,{...action.payload,quantity:1}]};
+   }if(action.type==="Increase"){
+       const IncreaseQuantityOfItems=[...state.selectedItems]
+       IncreaseQuantityOfItems[action.payload]={
+        ...IncreaseQuantityOfItems[action.payload], quantity: IncreaseQuantityOfItems[action.payload].quantity + 1
+       };
        
-    
-   }
+       
+       return {
+         ...state,
+      selectedItems: IncreaseQuantityOfItems
+       }
+    }
 }
 const CartContext = ({children,drawerHandeler}) => {
   const getInformation= useContext(ProductContext)
  const {products}=getInformation
-  
-//  const[selectedItems,setSelectedItems]=useState([])
-  
+    
     const [state,dispatch]=useReducer(reducer,initialArg)
   
    
   const showInfo =(Info)=>{
-    console.log(Info);
     const product = products.find(item => item.id === Info)
+    
     dispatch({type:"AddItem",payload:product})
           drawerHandeler() 
   }
 
-  
+  const increaseThequantity=(id)=>{
+    const productIndex=state.selectedItems.findIndex(item=> item.id === id)
+    dispatch({type:"Increase",payload:productIndex})
+    
+  }
+
   
   return (
-    <BuyContext value={{showInfo,selectedItems:state.selectedItems}}>
+    <BuyContext value={{showInfo,selectedItems:state.selectedItems,increaseThequantity}}>
         {children}
     </BuyContext>
   )
